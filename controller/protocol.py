18a6073ebcd95193bab2314166dfab3a8ec6cf41
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2019-10-22 15:59:30 -0300

    Start to implement Protocol

--- controller/protocol.py
+++ controller/protocol.py
@@ -1,2 +1,16 @@
+import serial
+
 class Protocol:
-    pass
+    def __init__(self, serial_port):
+        self.serial_port = serial_port
+    
+    def _connect(action="read"):
+        return serial.Serial(self.serial_port)
+
+    def get_sensors(self):
+        with self._connect() as conn:
+            conn.writelines("start")
+        with self._connect() as conn:
+            sensor_data = conn.read_until("endian")
+        with self._connect() as conn:
+            conn.writelines("stop")
--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from controller.plants import Plants
 
 plants = Plants()
 plants.set_info()
+
+protocol = Protocol("/dev/porta")
+protocol.get_sensors()
