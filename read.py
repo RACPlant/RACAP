c0f508555d1f5b55b7e7bf26ee77f54d035706d9
Susana Bouchardet <susana.bouchardet@gmail.com>
2019-10-29 17:23:49 -0300

    Comment the dataset load

--- read.py
+++ read.py
@@ -6,7 +6,7 @@ from controller.parser import Parser
 from controller.plants import Plants
 
 plants = Plants()
-plants.set_info()
+#plants.set_info()
 port = sys.argv[1] if (len(sys.argv) == 2) else "ACM0"
 
 protocol = Protocol("/dev/tty{}".format(port), Parser())
