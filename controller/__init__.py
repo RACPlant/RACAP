8a262ffb38b5770f5927d81e193db73c23865493
Susana Bouchardet <susana.bouchardet@gmail.com>
2019-11-05 17:47:30 -0300

    +1

--- controller/__init__.py
+++ controller/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=LOGLEVEL)
 
 def get_logger(name, **kwargs):
     fomatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = logging.FileHandler(f'{name}.log')
+    handler = logging.FileHandler('{}.log'.format(name))
     handler.setFormatter(fomatter)
     logger = logging.getLogger(name)
     logger.addHandler(handler)
