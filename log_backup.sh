609e8b55b33a7c7133107add6e739c7666518ec7
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2019-11-12 17:03:38 -0200

    Doing backup in folders

--- log_backup.sh
+++ log_backup.sh
@@ -2,7 +2,8 @@
 # run every 00:00:00
 
 LOG_FILES="Consumer*.log"
-
+LOG_DATE=$(date +%F_%T)
 echo Starting backup...
-mv $LOG_FILES /tmp/"$(date +%F_%T)/${LOG_FILE}"
+mkdir /tmp/${LOG_DATE}
+mv $LOG_FILES /tmp/"${LOG_DATE}/${LOG_FILE}"
 echo Process finished!
