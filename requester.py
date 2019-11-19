d3088e5e13940ce4faa1c957f3d670dd28e217f5
Susana Bouchardet <susana.bouchardet@gmail.com>
2019-11-19 10:25:47 -0300

    Waiting 2 minutes before start loop

--- requester.py
+++ requester.py
@@ -6,13 +6,15 @@ from controller import get_logger
 from social.devices import Arduino
 from social.serial_protocol import SerialProtocol
 
-SLEEP_MINUTES = 30
+SLEEP_MINUTES = 2
 
 logger = get_logger("Requester")
 
 arduinos = Arduino("api_endpoint")
 
 while True:
+    logger.info("Waiting {} minutes".format(SLEEP_MINUTES))
+    time.sleep(60 * SLEEP_MINUTES)
     logger.info("Start loop...")
     for arduino in arduinos.all:
         protocol = SerialProtocol(arduino["port"])
@@ -23,5 +25,3 @@ while True:
                 protocol.serial_port, SLEEP_MINUTES
                 ))
     logger.info("End loop.")
-    logger.info("Waiting {} minutes".format(SLEEP_MINUTES))
-    time.sleep(60 * SLEEP_MINUTES)
